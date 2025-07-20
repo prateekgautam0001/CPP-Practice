a47260f0e8c3525bfec28c1b0fd38bd9f2a044e5
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-07-20 11:48:56 +0530

    Add files via upload

---
+++ Variable.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //1. Integer Type
+    int a;
+    cout<<"Enter The Number: "<<endl;
+    cin>>a;
+    cout<<"The Number You Enter Is: "<<a;
+
+    //2. Float  Type
+    float b;
+    cout<<"Enter The Number: "<<endl;
+    cin>>b;
+    cout<<"The Number You Enter Is: "<<b;
+
+    //3. Character Type
+    char c;
+    cout<<"Enter The Character: "<<endl;
+    cin>>c;
+    cout<<"The Character You Enter Is: "<<c;
+
+    //4. Double Type
+    double d;
+    cout<<"Enter The Number: "<<endl;
+    cin>>d;
+    cout<<"The Number You Enter Is: "<<d;
+
+    
+    return 0;
+}
