24945edd9e789991dafd29e4b5ce71a8bd3a0056
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-07-23 11:53:07 +0530

    Add files via upload

---
+++ Basic-Star-Pattern.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int Row;
+    cout<<"Enter The Number of Rows: ";cin>>Row;
+    for(int i=0;i<Row;i++){
+        cout<<"* ";
+    }
+    return 0;
+}
