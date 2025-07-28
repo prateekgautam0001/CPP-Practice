bfb04a23b026b7f091aaabcf9d06a2b36d145fed
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-07-28 20:06:04 +0530

    Add files via upload

---
+++ Pointer.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=3;
+    int *b=&a;
+    cout<<"The Value Of a is:"<<a<<endl;
+    cout<<"The Address Of a is:"<<b<<endl;
+    return 0;
+}
