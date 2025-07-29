d74ccf4c5758832fdac8dae7c9b8949db6bc3f49
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-07-29 20:57:03 +0530

    Add files via upload

---
+++ Second-Largest-Element.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, max1, max2;
+    cout << "Enter size: ";
+    cin >> n;
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    max1 = max2 = INT_MIN;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > max1) {
+            max2 = max1;
+            max1 = arr[i];
+        } else if(arr[i] > max2 && arr[i] != max1) {
+            max2 = arr[i];
+        }
+    }
+
+    cout << "Second largest element: " << max2;
+    return 0;
+}
