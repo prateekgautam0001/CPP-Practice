09d3521344237f7327b6e5c4e09c631ddc20ac52
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-07-31 20:40:25 +0530

    Add files via upload

---
+++ Basic-Enum-Example.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+enum Color { RED, GREEN, BLUE };
+
+int main() {
+    Color c = GREEN;
+    cout << "Color value: " << c << endl;  // prints 1
+    return 0;
+}
