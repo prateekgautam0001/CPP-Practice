d616b61aff8cef889ad6925ed7af19443d5c70fa
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-08-07 20:38:29 +0530

    Print Numbers in Reverse Order (n to 1).cpp

---
+++ Print Numbers in Reverse Order (n to 1).cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void printReverse(int n) {
+    if(n == 0) return;
+    cout << n << " ";
+    printReverse(n - 1);
+}
+
+int main() {
+    printReverse(5);
+}
