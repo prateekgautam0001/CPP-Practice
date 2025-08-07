5f47808cc5a36fb244f7a7bbac7c94fb35360b9c
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-08-07 20:38:03 +0530

    Sum of Digits using Recursion.cpp

---
+++ Sum of Digits using Recursion.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int digitSum(int n) {
+    if(n == 0) return 0;
+    return n % 10 + digitSum(n / 10);
+}
+
+int main() {
+    cout << "Digit Sum = " << digitSum(123);
+}
