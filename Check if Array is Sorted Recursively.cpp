18b9b0ba921da3bb6954abe3efbc574fd325c58a
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-08-07 20:43:25 +0530

    Check if Array is Sorted Recursively.cpp

---
+++ Check if Array is Sorted Recursively.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+bool isSorted(int arr[], int n) {
+    if(n == 1) return true;
+    return arr[0] <= arr[1] && isSorted(arr + 1, n - 1);
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    cout << (isSorted(arr, 5) ? "Sorted" : "Not Sorted");
+}
