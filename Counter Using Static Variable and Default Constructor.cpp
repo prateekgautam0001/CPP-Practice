5ce4771c9a63f82dc77715cd17b429b183fa1960
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-08-30 07:40:12 +0530

    Counter Using Static Variable and Default Constructor.cpp

---
+++ Counter Using Static Variable and Default Constructor.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Counter {
+private:
+    static int count;
+
+public:
+    Counter() {
+        count++;
+    }
+
+    static void showCount() {
+        cout << "Count: " << count << endl;
+    }
+};
+
+int Counter::count = 0;
+
+int main() {
+    Counter c1, c2, c3;
+    Counter::showCount();
+    return 0;
+}
