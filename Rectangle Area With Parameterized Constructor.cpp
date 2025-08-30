40061779420b537e892800a330c4d0f1c5db222b
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-08-30 07:52:56 +0530

    Rectangle Area With Parameterized Constructor.cpp

---
+++ Rectangle Area With Parameterized Constructor.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+private:
+    int length, width;
+
+public:
+    Rectangle(int l, int w) {
+        length = l;
+        width = w;
+    }
+
+    int area() {
+        return length * width;
+    }
+};
+
+int main() {
+    Rectangle r(5, 3);
+    cout << "Area: " << r.area() << endl;
+    return 0;
+}
