b87f900278de35d5903e709fec69a46ed91cbc8c
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-08-31 07:42:13 +0530

    Basic Destructor Trace.cpp

---
+++ asic Destructor Trace.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Test {
+public:
+    Test() {
+        cout << "Constructor called" << endl;
+    }
+    ~Test() {
+        cout << "Destructor called" << endl;
+    }
+};
+
+int main() {
+    Test t;
+    return 0;
+}
