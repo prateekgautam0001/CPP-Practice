d34c90cf0779205efd1e7a33927394261e7f518a
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-09-01 08:25:47 +0530

    Constructor and Destructor Call Order Using Single Inheritance.cpp

---
+++ Constructor and Destructor Call Order Using Single Inheritance.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    Base() {
+        cout << "Base Constructor\n";
+    }
+    ~Base() {
+        cout << "Base Destructor\n";
+    }
+};
+
+class Derived : public Base {
+public:
+    Derived() {
+        cout << "Derived Constructor\n";
+    }
+    ~Derived() {
+        cout << "Derived Destructor\n";
+    }
+};
+
+int main() {
+    Derived d;
+    return 0;
+}
