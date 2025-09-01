07a89300536b333bfeede5bd473c6624338c5e25
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-09-01 08:55:02 +0530

    Ambiguity Without Virtual — Then Fixed.cpp

---
+++ Ambiguity Without Virtual — Then Fixed.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    void sayHello() {
+        cout << "Hello from A\n";
+    }
+};
+
+class B : virtual public A {};
+class C : virtual public A {};
+class D : public B, public C {};
+
+int main() {
+    D obj;
+    obj.sayHello();
+    return 0;
+}
