8f5790da6df8a480c128ba508c64b2c1b7734f94
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-09-02 07:45:08 +0530

    Multiple Inheritance Constructor Call.cpp

---
+++ Multiple Inheritance Constructor Call.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    A() { cout << "A\n"; }
+};
+
+class B {
+public:
+    B() { cout << "B\n"; }
+};
+
+class C : public A, public B {
+public:
+    C() { cout << "C\n"; }
+};
+
+int main() {
+    C c;
+    return 0;
+}
