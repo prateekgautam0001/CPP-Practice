e3c70332141b779f108c01be67de12a06a104594
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-09-02 07:42:56 +0530

    Simple Constructor in Base and Derived Class.cpp

---
+++ Simple Constructor in Base and Derived Class.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    Base() { cout << "Base Constructor\n"; }
+};
+
+class Derived : public Base {
+public:
+    Derived() { cout << "Derived Constructor\n"; }
+};
+
+int main() {
+    Derived d;
+    return 0;
+}
