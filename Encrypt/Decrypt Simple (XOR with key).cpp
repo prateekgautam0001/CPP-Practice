2f440fc5ba6f5341ed89d0c5795f56a985153ccd
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-09-03 08:22:23 +0530

    Delete Encrypt directory

--- Encrypt/Decrypt Simple (XOR with key).cpp
+++
@@ -1,9 +0,0 @@
-#include <fstream>
-#include <iostream>
-int main(){
-    char c;
-    std::ifstream in("plain.txt", std::ios::binary);
-    std::ofstream out("cipher.bin", std::ios::binary);
-    char key=0xFF;
-    while(in.get(c)) out.put(c ^ key);
-}
