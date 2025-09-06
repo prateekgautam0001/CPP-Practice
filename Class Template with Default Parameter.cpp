1882b5d38db845dc4304b11460433ed4cd73abf7
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-09-06 07:32:40 +0530

    Class Template with Default Parameter.cpp

---
+++ Class Template with Default Parameter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+template <typename T = int>
+class Number {
+    T num;
+public:
+    Number(T n) : num(n) {}
+    void show() { cout << num << endl; }
+};
+
+int main() {
+    Number<> n1(100);
+    Number<float> n2(3.14);
+    n1.show();
+    n2.show();
+}
