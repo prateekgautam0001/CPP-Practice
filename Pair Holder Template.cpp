59500109e5bbbad8378e2f5c841cb6f45aa1624a
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-09-06 07:47:34 +0530

    Pair Holder Template.cpp

---
+++ Pair Holder Template.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+template <typename T1, typename T2>
+class Pair {
+    T1 first;
+    T2 second;
+public:
+    Pair(T1 a, T2 b) : first(a), second(b) {}
+    void display() { cout << first << " " << second << endl; }
+};
+
+int main() {
+    Pair<int, string> p(1, "Hello");
+    p.display();
+}
