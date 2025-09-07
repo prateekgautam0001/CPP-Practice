23f8f364fae734eda92f42603453fd1abbe59a77
Prateek Gautam <145118104+prateekgautam0001@users.noreply.github.com>
2025-09-07 07:07:48 +0530

    Reverse a vector.cpp

---
+++ Reverse a vector.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<int> v = {1, 2, 3, 4};
+    reverse(v.begin(), v.end());
+    for(int x : v) cout << x << " ";
+}
